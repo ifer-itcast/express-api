4c335a94765066be95b980d5823e712b1f909f40
ifer-itcast <yangkangkang@itcast.cn>
2020-11-10 16:18:37 +0800

    编写POST接口

--- apiRouter.js
+++ apiRouter.js
@@ -14,4 +14,16 @@ router.get('/get', (req, res) => {
     })
 });
 
+// 定义 POST 接口
+router.post('/post', (req, res) => {
+    // 通过 req.body 获取请求体中包含的 url-encoded 格式的数据
+    const body = req.body
+    // 调用 res.send() 方法，向客户端响应结果
+    res.send({
+        status: 0,
+        msg: 'POST 请求成功！',
+        data: body,
+    })
+});
+
 module.exports = router;
--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const express = require('express');
 // 创建服务器对象
 const app = express();
 
+// 解析前端传递过来的数据，并且把解析好的结果挂载到 req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // 加载 /api 前缀，凡是 /api 开头的都去 ./apiRouter 里面继续匹配
 app.use('/api', require('./apiRouter'));
 
